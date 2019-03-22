d06fa087468d9405a9a513bf4ee4eadda65881f3
bxs9775 <bxs9775@rit.edu>
2019-03-22 10:35:05 -0400

    Starting project files

---
+++ IGME-451_RegEx_HW/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// IGME-451_RegEx_HW.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
