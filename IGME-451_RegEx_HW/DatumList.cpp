f70041b801fa04eb3d51efaac0e8cd48d56d413e
bxs9775 <bxs9775@rit.edu>
2019-03-31 19:38:11 -0400

    Revert "Apparently giving DatumBase a std::string name feild causes access"
    This reverts commit e7574e2582e5cf4036d63bc647f95ec31a72d1cb.

--- IGME-451_RegEx_HW/DatumList.cpp
+++ IGME-451_RegEx_HW/DatumList.cpp
@@ -2,7 +2,7 @@
 #include "DatumList.h"
 
 
-DatumList::DatumList(std::string name, ConfigVar::configVar vType, std::list<Datum> val):DatumBase(name, vType)
+DatumList::DatumList(ConfigVar::configVar vType, std::list<Datum> val):DatumBase(vType)
 {
 	value = val;
 }
@@ -19,16 +19,15 @@ std::list<Datum> DatumList::getValue()
 
 void DatumList::print(std::ostream& output)
 {
-	output << "DatumList - " << getName() << ":" << std::endl;
-	//output << "DatumList:" << std::endl;
+	output << "DatumList object:" << std::endl;
 	//output << " -- type: " << ConfigVar::conVarNames(getVarType()) << std::endl;
 	output << " -- value: [";
-	std::list<Datum> val = getValue();
-	std::list<Datum>::iterator iter = val.begin();
+	std::list<Datum> value = getValue();
+	std::list<Datum>::iterator iter = value.begin();
 	while (iter != value.end()) {
 		output << iter->getValue();
 		iter++;
-		if (iter != val.end()) {
+		if (iter != value.end()) {
 			output << ", ";
 		}
 	}
